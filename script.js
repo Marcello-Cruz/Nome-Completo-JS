0654c0f7fdb640695c2f806d97d8e920a3a14e98
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-05-26 19:41:08 -0300

    add exerc nm completo JS

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = prompt('digite seu nome:');
+let sobrenome = prompt('digite seu sobrenome:');
+
+alert(`Nome completo: ${nome.toUpperCase()} ${sobrenome}`);
+alert(`Nome de catálogo: ${sobrenome.toUpperCase()} ${nome}`);
