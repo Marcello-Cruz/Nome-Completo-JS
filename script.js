4c1c8005ed5f32ab8ae08471630fd73d66872c21
Marcello-Cruz <marcellinhopastelzinho@gmail.com>
2022-08-16 20:18:22 -0300

    correct account

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let nome = prompt('digite seu nome:');
-let sobrenome = prompt('digite seu sobrenome:');
+let nome = prompt('Digite seu Nome:');
+let sobrenome = prompt('Digite seu Sobrenome:');
 
 alert(`Nome completo: ${nome.toUpperCase()} ${sobrenome}`);
 alert(`Nome de catálogo: ${sobrenome.toUpperCase()} ${nome}`);
